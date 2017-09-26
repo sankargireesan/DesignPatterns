b25af8f3190f625a0a8633ee982cf09d879cc0a6
Sankar Gireesan Nair <sankargireesan@ccs.neu.edu>
2017-09-25 20:11:42 -0400

    updating java files to make it print all numbers

--- HW1/PrintNumbers.java
+++ HW1/PrintNumbers.java
@@ -3,21 +3,8 @@ public class PrintNumbers {
     public static void main(String[] args){
         int firstNum = Integer.min(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
         int secNum = Integer.max(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
-
-
-
-
-
-       
-
-     
-	if((firstNum % 2)==0){
-		firstNum+=2;
-	}else{
-		firstNum++;
-	}
 	
-	for(int i = firstNum; i < secNum; i+=2)
+	for(int i = firstNum+1; i < secNum; i++)
 
             System.out.println(i);
 
